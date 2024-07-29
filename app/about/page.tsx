d309cc06b442c45e0297e1b60d3d133186fec5be
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-07-29 23:11:04 +0200

    Refactor - About page design. Add -icons

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,71 @@
+import Image from "next/image"
+import Link from "next/link"
+
 export default function About() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-2">
-      <div>
-        <p>About Page!</p>
+    <main className="flex min-h-screen flex-col items-center justify-start">
+      <div className="bg-black">
+        <Image
+          src="/andreas_profil.jpg"
+          alt="Profile picture of Andreas A"
+          width={400}
+          height={400}
+          className="rounded-full p-3"
+        />
+        <div className="p-3 bg-indigo-400 drop-shadow-xl">
+          <h2 className="text-center text-2xl font-semibold m-3">
+            Andreas Antonsson
+          </h2>
+          <h3 className="mb-4 tracking-widest text-center font-light">
+            App & Webb Development
+          </h3>
+          <div className="mb-4 flex justify-between">
+            <Link
+              href="https://www.linkedin.com/in/andreas-antonsson-12209024b/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/linkedin_icon.png"
+                alt="LinkedIn Icon"
+                width={40}
+                height={40}
+                className="rounded-md bg-slate-50 drop-shadow-lg"
+              />
+            </Link>
+            <p className="text-center mb-2">________________________</p>
+            <Link
+              href="https://github.com/rhiz0m"
+              target="blank"
+              rel="noopener noreferrner"
+            >
+              <Image
+                src="/github_icon.png"
+                alt="Github Icon"
+                width={40}
+                height={40}
+                className="rounded-full bg-white drop-shadow-lg"
+              />
+            </Link>
+          </div>
+
+          <div className="flex items-center justify-center">
+            <p className="font-extralight text-sm mr-2">created with: </p>
+            <Image
+              src="/next.svg"
+              alt="A Next Icon"
+              width={50}
+              height={20}
+              className="mr-4"
+            />
+            <Image
+              src="/vercel.svg"
+              alt="A Vercel Icon"
+              width={50}
+              height={20}
+            />
+          </div>
+        </div>
       </div>
     </main>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { useState } from "react"
 import About from "../app/about/page"
 import Contact from "../app/contact/page"
 import Projects from "../app/projects/page"
+import Image from "next/image"
+
 
 export default function Home() {
   const [content, setContent] = useState("")
@@ -21,40 +23,55 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-start justify-start p-2">
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left bg-white">
+    <main className="flex min-h-screen flex-col items-stretch justify-start">
+      <div className="mb-32 grid w-full lg:mb-0 lg:w-full lg:grid-cols-6 bg-indig p-1">
         <button
           onClick={() => setContent("about")}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-emerald-500 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-indigo-500 hover:bg-indigo-400 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
+          <h2 className="mb-2 text-2xl font-semibold">
             About{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+              <Image
+                src="/about_icon.png"
+                alt="Arrow Icon"
+                width={20}
+                height={20}
+              />
             </span>
           </h2>
         </button>
 
         <button
           onClick={() => setContent("contact")}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-emerald-500 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-indigo-500 hover:bg-indigo-400 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
+          <h2 className="mb-2 text-2xl font-semibold">
             Contact{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+              <Image
+                src="/contact_icon.png"
+                alt="Contact Icon"
+                width={20}
+                height={20}
+              />
             </span>
           </h2>
         </button>
 
         <button
           onClick={() => setContent("projects")}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-emerald-500 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-indigo-500 hover:bg-indigo-400 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
+          <h2 className="mb-2 text-2xl font-semibold">
             Projects{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+              <Image
+                src="/projects_icon.png"
+                alt="Projects Icon"
+                width={20}
+                height={20}
+              />
             </span>
           </h2>
         </button>
